deaf580e1c3c0e3bac7eb31d18164f2c0d790038
Halejandro_Oliveira <halejandroalvesoliveira10@gmail.com>
2024-10-15 21:10:42 -0300

    popup update

--- scripts/geral.js
+++ scripts/geral.js
@@ -7,10 +7,24 @@ console.error("Eu te odeio temoteo!!!!!!!!!!!!!!!!!");
 button.addEventListener('click', ()=>{
     popup.showModal();
     const stars = document.querySelectorAll('.star');
+    const send = document.getElementById('send-btn');
+    const close = document.getElementById('close')
+    let isEvaluated = false;
+
+    close.addEventListener('click',()=>popup.close())
+    send.addEventListener('click',()=>{
+        if(isEvaluated){
+            alert('Obrigado pela avaliação!');
+            popup.close();
+            return;
+        }
+        alert('Avalie antes de enviar')
+    })
     stars.forEach(star => {
         star.addEventListener('click',()=>{
             const rating = star.getAttribute('data-value');
             stars.forEach(s => s.classList.remove('active'));
+            isEvaluated = true;
             for (let i = 0; i < rating; i++) {
                 stars[i].classList.add('active')
             }
